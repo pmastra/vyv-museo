55d166a131a49532a4420cf6f150cc665a404bd3
Pmastra <pabloo.agustin@gmail.com>
2019-07-02 17:34:36 -0300

    Se suben tests mas algunas modificaciones

--- museoApi/test/testExcavacion.js
+++ museoApi/test/testExcavacion.js
@@ -13,6 +13,21 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+
+describe ('GET Excavacion' , () => {
+    it('Deberia retornarnos un 200 mas todas las excavaciones que se encuentren', (done) => {
+        chai.request(servidor)
+            .get('/api/excavacion')
+            //.send(excavacion.idExcavacion)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.Excavacion.should.have.property('_id');
+                done();
+            });
+    });
+});
 describe('GET Excavacion por id', () => {
 
     //before((done) => {
@@ -28,10 +43,10 @@ describe('GET Excavacion por id', () => {
 
     it('Id correcta, deberia retornarnos un 200 mas su id de mongo', (done) => {
         let excavacion = {
-            idExcavacion: "5d129ea9bacbe8218449f4b9"
+            idExcavacion: "5d1bafa0c85fe83854f6a667"
         }
         chai.request(servidor)
-            .get('/api/excavacionId/5d129ea9bacbe8218449f4b9')
+            .get('/api/excavacionId/5d1bafa0c85fe83854f6a667')
             //.send(excavacion.idExcavacion)
             .end((err, res) => {
                 res.should.have.status(200);
@@ -108,6 +123,21 @@ describe('POST Excavacion', () =>{
     it('Esto deberia retornar un status 500 insertando una excavacion sin un id de exploracion', (done) =>{
         chai.request(servidor)
             .post('/api/areaExcavacion')
+            .send()
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.Excavacion.should.have.property('_id');
+                done();
+        });
+    });
+    it('Esto deberia retornar un status 500 insertando una excavacion sin un id de mongo correcto', (done) =>{
+        let excavacion = {
+            exploracionId: "5d129572161e811c3410725r" //id de exploracion incorrecto para mongo, nos vamos del limite
+        }
+        chai.request(servidor)
+            .post('/api/areaExcavacion')
             .send(excavacion)
             .end((err, res) => {
                 res.should.have.status(500);
@@ -116,5 +146,19 @@ describe('POST Excavacion', () =>{
                 res.body.Excavacion.should.have.property('_id');
                 done();
         });
+    });   
+});
+
+describe('DELETE excavacion',()=>{
+    it('Deberia retornar (200) al borrar todas las excavaciones', (done) => {
+        chai.request(servidor)
+        .delete('/api/areaExcavacion')
+        .end((err, res) => {
+            res.should.have.status(500);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.Excavacion.should.have.property('_id');
+            done();
+         });
     });
 });
